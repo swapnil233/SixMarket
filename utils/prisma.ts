25e0f8e9147ab5a15df62dfa880769d719d096b8
Hasan Iqbal <swapniliqbal@gmail.com>
2023-04-14 18:34:42 -0400

    Prisma client and connection pooling optimization

--- utils/prisma.ts
+++ utils/prisma.ts
@@ -1,4 +1,15 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+import { PrismaClient } from '@prisma/client'
 
-export default prisma;
+const globalForPrisma = global as unknown as {
+    prisma: PrismaClient | undefined
+}
+
+const prisma =
+    globalForPrisma.prisma ??
+    new PrismaClient({
+        log: ['query'],
+    })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export default prisma
