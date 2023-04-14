3cc5888c0fdda6fd3fad46503e20fe715d9054f9
Hasan Iqbal <swapniliqbal@gmail.com>
2023-04-13 21:07:08 -0400

    Layout wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import "@/styles/globals.css";
 import Layout from "@/components/Layout";
+import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
@@ -11,7 +11,9 @@ export default function App({
 }: AppProps & { session: Session }) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </SessionProvider>
   );
 }
