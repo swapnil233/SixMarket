f738e484ea69fa30692036a89aaf46d98723b7ac
Hasan Iqbal <swapniliqbal@gmail.com>
2023-04-16 02:42:35 -0400

    Custom logger wrapper using Morgan

---
+++ utils/logger.ts
@@ -0,0 +1,9 @@
+const morgan = require('morgan');
+
+// Choose a format (combined, common, dev, short, or tiny) or create a custom format
+const format = 'combined';
+
+// Create the logger middleware
+const logger = morgan(format);
+
+module.exports = logger;
