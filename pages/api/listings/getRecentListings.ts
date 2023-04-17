fe0d9db0a52970aab1c08ebfdc9b1a5b5936ccca
Hasan Iqbal <swapniliqbal@gmail.com>
2023-04-16 21:37:05 -0400

    New API endpoint to get 10 recent listings/ads

---
+++ pages/api/listings/getRecentListings.ts
@@ -0,0 +1,44 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from '@/utils/prisma';
+import { ItemForSale, User } from '@prisma/client';
+const logger = require('@/utils/logger'); // Import the logger middleware using the 'require' syntax
+
+async function getTenRecentAds(): Promise<ItemForSale[] | null> {
+    try {
+        const listings = await prisma.itemForSale.findMany({
+            orderBy: {
+                createdAt: 'desc',
+            },
+            take: 10,
+        });
+        return listings;
+    } catch (error) {
+        throw new Error('Failed to fetch data');
+    }
+}
+
+export default async function Handler(req: NextApiRequest, res: NextApiResponse) {
+    const id = req.query.id as string;
+
+    await logger(req, res, async () => {
+        try {
+            const recentlyPostedAds = await getTenRecentAds();
+
+            if (!recentlyPostedAds || recentlyPostedAds.length === 0) {
+                res.status(404).send({ error: 'No listings found' });
+                return;
+            }
+
+            // If the request method is HEAD, only send the status
+            if (req.method === 'HEAD') {
+                res.status(200).end();
+                return;
+            }
+
+            // If the request method is not HEAD, send the user data
+            res.status(200).json(recentlyPostedAds);
+        } catch (error: any) {
+            res.status(500).send({ error: error.message });
+        }
+    });
+}
