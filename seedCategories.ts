24c4bdc4c906295c8e4e5db248ab89903847ad8f
Hasan Iqbal <swapniliqbal@gmail.com>
2023-04-18 19:00:47 -0400

    Updated seeding script to include more categories

--- seedCategories.ts
+++ seedCategories.ts
@@ -4,10 +4,22 @@ const prisma = new PrismaClient();
 async function main() {
     console.log("Initiated seeding")
     const categories = [
-        { name: 'Electronics', slug: 'electronics' },
-        { name: 'Furniture', slug: 'furniture' },
-        { name: 'Vehicles', slug: 'vehicles' },
-        { name: 'Clothing', slug: 'clothing' },
+        { name: 'Services', slug: 'services' },
+        { name: 'Jobs', slug: 'jobs' },
+        { name: 'Pets', slug: 'pets' },
+        { name: 'Sporting Goods', slug: 'sporting-goods' },
+        { name: 'Collectibles', slug: 'collectibles' },
+        { name: 'Books & Magazines', slug: 'books-magazines' },
+        { name: 'Toys & Games', slug: 'toys-games' },
+        { name: 'Musical Instruments', slug: 'musical-instruments' },
+        { name: 'Farm & Garden', slug: 'farm-garden' },
+        { name: 'Health & Beauty', slug: 'health-beauty' },
+        { name: 'Baby & Kids', slug: 'baby-kids' },
+        { name: 'Free Stuff', slug: 'free-stuff' },
+        { name: 'Tickets', slug: 'tickets' },
+        { name: 'Travel & Vacation', slug: 'travel-vacation' },
+        { name: 'Community', slug: 'community' },
+        { name: 'Lost & Found', slug: 'lost-found' },
     ];
 
     // Insert the categories into the database
@@ -15,7 +27,7 @@ async function main() {
         await prisma.category.create({
             data: category,
         });
-        console.log("Successfully inserted " + category)
+        console.log("Successfully inserted " + category.name)
     }
 }
 
