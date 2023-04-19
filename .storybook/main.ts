0e6353e031baadee401003b26d730c012cda9380
Hasan Iqbal <swapniliqbal@gmail.com>
2023-04-19 10:07:25 -0400

    refactor: deleted the example stories that comes with storybook init

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,6 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 const config: StorybookConfig = {
-  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)", '../components/*.stories.mdx', '../components/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../components/*.stories.mdx', '../components/*.stories.@(js|jsx|ts|tsx)'],
   /** Expose public folder to storybook as static */
   staticDirs: ['../public'],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
