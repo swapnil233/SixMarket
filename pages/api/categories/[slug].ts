c96da0d7deca2ec362a809e4bfdf404fbf38bb2a
Hasan Iqbal <swapniliqbal@gmail.com>
2023-04-24 09:57:37 -0400

    feat: new API and page to get and show a category by its slug

---
+++ pages/api/categories/[slug].ts
@@ -0,0 +1,63 @@
+import prisma from "@/utils/prisma";
+import { Category, Listing } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export interface IGetCategoryAndAssociatedListingsResponse {
+    category: Category;
+    allListingsInCategory: Array<Listing>;
+}
+
+async function getCategory(slug: string) {
+    return await prisma.category.findUnique({
+        where: {
+            slug: slug,
+        },
+    });
+}
+
+async function getListingsByCategoryId(categoryId: string) {
+    return await prisma.listing.findMany({
+        where: {
+            categoryId: categoryId,
+        },
+        include: {
+            images: true,
+            tags: true,
+            favorites: true,
+        },
+    });
+}
+
+// '/api/categories/'
+export default async function Handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === "GET") {
+        const slug: string = req.query.slug as string;
+
+        // Input validation
+        if (!slug) {
+            return res.status(400).send("Missing 'slug' parameter in the request.");
+        }
+
+        try {
+            const category = await getCategory(slug);
+
+            if (!category) {
+                return res.status(400).send(`Category "${slug}" doesn't exist.`);
+            }
+
+            const allListingsInCategory = await getListingsByCategoryId(category.id);
+
+            const response: IGetCategoryAndAssociatedListingsResponse = {
+                category,
+                allListingsInCategory,
+            };
+
+            return res.status(200).json(response);
+        } catch (error: any) {
+            res.status(500).json({ message: "An error occurred.", error: error.message });
+        }
+    } else {
+        res.setHeader("Allow", "GET");
+        res.status(405).send("Method not allowed. Use 'GET' instead.");
+    }
+}
--- pages/categories/[slug].tsx
+++ pages/categories/[slug].tsx
@@ -1,7 +1,43 @@
 import PrimaryLayout from "@/components/layout/primary/PrimaryLayout";
+import { Category, Listing } from "@prisma/client";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { IGetCategoryAndAssociatedListingsResponse } from "../api/categories/[slug]";
 import { NextPageWithLayout } from "../page";
 
 const Home: NextPageWithLayout = () => {
+  const router = useRouter();
+  const { slug } = router.query;
+
+  const [categoryInfo, setCategoryInfo] = useState<Category>({
+    id: "",
+    name: "",
+    slug: "",
+    thumbnail: "",
+  });
+  const [listingsInCategory, setListingsInCategory] = useState<Listing[]>([]);
+
+  useEffect(() => {
+    const fetchCategoryAndListingsData = async () => {
+      try {
+        const res = await axios.get<IGetCategoryAndAssociatedListingsResponse>(
+          `/api/categories/${slug}`
+        );
+
+        setCategoryInfo(res.data.category);
+        setListingsInCategory(res.data.allListingsInCategory);
+      } catch (error: any) {
+        console.error(
+          "Error fetching category and listings data:",
+          error.message
+        );
+      }
+    };
+
+    slug && fetchCategoryAndListingsData();
+  }, [slug]);
+
   return (
     <div>
       {/* Hero */}
@@ -11,7 +47,7 @@ const Home: NextPageWithLayout = () => {
             {/* Title */}
             <div className="md:max-w-5xl mx-auto">
               <h1 className="text-5xl lg:text-6xl mb-4 font-normal">
-                Category Name
+                {categoryInfo.name}
               </h1>
             </div>
           </section>
