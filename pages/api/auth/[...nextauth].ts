766d31ef575b720fa7a6a396b4f6d28f4ed94ee3
Hasan Iqbal <swapniliqbal@gmail.com>
2023-04-23 23:36:05 -0400

    refactor: attach user ID to session token to avoid a DB call

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,19 +1,46 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
 const prisma = new PrismaClient();
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
+
+  // Google Provider for now
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+
   secret: process.env.JWT_SECRET,
+
+  // Longer session than the default
+  session: {
+    strategy: "database",
+    maxAge: 60 * 60 * 24 * 30, // 30 days
+    updateAge: 60 * 60 * 24, // 24 hours
+  },
+
+  // https://next-auth.js.org/configuration/callbacks
+  callbacks: {
+    // Attach the user's ID to the session so Prisma can access it
+    async session({ session, user }) {
+      if (session?.user) {
+        return {
+          ...session,
+          user: {
+            ...session.user,
+            id: user.id
+          },
+        };
+      }
+      return session
+    },
+  }
 };
 
 export default NextAuth(authOptions);
