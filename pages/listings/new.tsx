19255ef94d9513481f5e4fe094ac42bd30b863b6
Hasan Iqbal <swapniliqbal@gmail.com>
2023-04-23 23:37:37 -0400

    feat: new dropzone for images in the new listing page

--- pages/listings/new.tsx
+++ pages/listings/new.tsx
@@ -5,15 +5,20 @@ import {
   Button,
   Divider,
   FileInput,
+  Group,
   MultiSelect,
   NumberInput,
   Radio,
   Select,
+  Text,
   TextInput,
   Textarea,
+  rem,
 } from "@mantine/core";
+import { Dropzone, IMAGE_MIME_TYPE } from "@mantine/dropzone";
 import { useForm } from "@mantine/form";
 import { Category, Tag } from "@prisma/client";
+import { IconPhoto, IconUpload, IconX } from "@tabler/icons-react";
 import axios, { AxiosResponse } from "axios";
 import { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
@@ -155,6 +160,39 @@ const CreateNewAd: NextPageWithLayout<CreateNewAdProps> = ({ apiUrl }) => {
           Please provide your listing/ad details
         </p>
         {/* File input for pics */}
+        <Dropzone
+          onDrop={(files) => console.log("accepted files", files)}
+          onReject={(files) => console.log("rejected files", files)}
+          maxSize={3 * 1024 ** 2}
+          accept={IMAGE_MIME_TYPE}
+          // {...props}
+        >
+          <Group
+            position="center"
+            spacing="xl"
+            style={{ minHeight: rem(220), pointerEvents: "none" }}
+          >
+            <Dropzone.Accept>
+              <IconUpload size="3.2rem" stroke={1.5} />
+            </Dropzone.Accept>
+            <Dropzone.Reject>
+              <IconX size="3.2rem" stroke={1.5} />
+            </Dropzone.Reject>
+            <Dropzone.Idle>
+              <IconPhoto size="3.2rem" stroke={1.5} />
+            </Dropzone.Idle>
+
+            <div>
+              <Text size="xl" inline>
+                Drag images here or click to select files
+              </Text>
+              <Text size="sm" color="dimmed" inline mt={7}>
+                Attach as many files as you like, each file should not exceed
+                5mb
+              </Text>
+            </div>
+          </Group>
+        </Dropzone>
         <FileInput
           label="Upload photos"
           maw={400}
