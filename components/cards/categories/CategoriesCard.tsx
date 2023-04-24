cfbaa4bc97df47ae8d2526d833daca648cd93e01
Hasan Iqbal <swapniliqbal@gmail.com>
2023-04-23 23:41:36 -0400

    refactor: remove unused values

--- components/cards/categories/CategoriesCard.tsx
+++ components/cards/categories/CategoriesCard.tsx
@@ -1,44 +1,7 @@
-import {
-  Card,
-  Group,
-  Text,
-  createStyles,
-  getStylesRef,
-  rem,
-} from "@mantine/core";
+import { Card, Group, Text } from "@mantine/core";
 import Image from "next/image";
 import Link from "next/link";
 import { FC } from "react";
-
-const useStyles = createStyles((theme) => ({
-  price: {
-    color: theme.colorScheme === "dark" ? theme.white : theme.black,
-  },
-
-  carousel: {
-    "&:hover": {
-      [`& .${getStylesRef("carouselControls")}`]: {
-        opacity: 1,
-      },
-    },
-  },
-
-  carouselControls: {
-    ref: getStylesRef("carouselControls"),
-    transition: "opacity 150ms ease",
-    opacity: 0,
-  },
-
-  carouselIndicator: {
-    width: rem(4),
-    height: rem(4),
-    transition: "width 250ms ease",
-
-    "&[data-active]": {
-      width: rem(16),
-    },
-  },
-}));
 
 export interface ICategoriesCard {
   thumbnail: string;
@@ -51,11 +14,6 @@ const CategoriesCard: FC<ICategoriesCard> = ({ title, thumbnail, slug }) => {
     <Link href={`/${slug}`} className="text-decoration-none no-underline">
       <Card radius="md" withBorder padding="sm" h={"100%"}>
         <Card.Section>
-          {/* <Image
-            src={thumbnail}
-            height={130}
-            alt={`Image for the ${title} category.`}
-          /> */}
           <Image
             height={130}
             width={210}
