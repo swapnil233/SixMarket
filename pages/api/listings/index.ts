391937a38296e6fcc6eedf8d79ab8a65a067eb02
Hasan Iqbal <swapniliqbal@gmail.com>
2023-04-23 23:37:12 -0400

    refactor: begun refactoring listings API

---
+++ pages/api/listings/index.ts
@@ -0,0 +1,53 @@
+import prisma from "@/utils/prisma";
+import { Listing } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+import { Session, getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
+
+async function CreateNewAd(adData: Listing) {
+    try {
+        const newAd = await prisma.listing.create({
+            data: adData,
+        });
+        return newAd;
+    } catch (error: any) {
+        throw new Error(error);
+    }
+}
+
+// POST '/api/listings/'
+export default async function Handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    const session: Session | null = await getServerSession(req, res, authOptions);
+
+    if (session) {
+        // POST '/api/listings/'
+        if (req.method === "POST") {
+            console.log("sdfs")
+            const userId = session.user?.id;
+            console.log(userId)
+            try {
+                // const adData = {
+                //     userId,
+                //     ...req.body,
+                //     price: parseInt(req.body.price, 10),
+                // };
+
+                // console.log("Data from server", adData);
+                // const newAd = await CreateNewAd(adData);
+
+                res.status(201);
+            } catch (error: any) {
+                console.error("API error:", error); // Add this line to log the error
+                res.status(500).send({ error: error.message });
+            }
+        } else {
+            res.setHeader("Allow", "POST");
+            res.status(405).end("Method Not Allowed");
+        }
+    } else {
+        res.status(401).send("401 - Not Authorized");
+    }
+}
