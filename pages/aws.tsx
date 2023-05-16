d188064b12ac3a6e0014a8805c004982246e5b7e
Hasan Iqbal <swapniliqbal@gmail.com>
2023-05-16 02:09:59 -0400

    feat: implement basic AWS S3 image uploading feature

--- pages/aws.tsx
+++ pages/aws.tsx
@@ -1,12 +1,13 @@
-import { NextPageWithLayout } from "@/pages/page";
 import PrimaryLayout from "@/components/layout/primary/PrimaryLayout";
-import { useState } from "react";
-import { FileInput, rem } from "@mantine/core";
-import { IconUpload } from "@tabler/icons-react";
-import { GetServerSideProps, GetServerSidePropsContext } from "next";
-import { requireAuthentication } from "@/utils/requireAuthentication";
-import { User } from "@prisma/client";
+import { NextPageWithLayout } from "@/pages/page";
 import prisma from "@/utils/prisma";
+import { requireAuthentication } from "@/utils/requireAuthentication";
+import { Button, FileInput, rem } from "@mantine/core";
+import { User } from "@prisma/client";
+import { IconUpload } from "@tabler/icons-react";
+import axios from "axios";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
+import { useState } from "react";
 
 export const getServerSideProps: GetServerSideProps = async (
   context: GetServerSidePropsContext
@@ -24,12 +25,49 @@ export const getServerSideProps: GetServerSideProps = async (
   });
 };
 
-const AWSTest: NextPageWithLayout = () => {
+interface AWSTestPageProps {
+  user: User | null;
+}
+
+const AWSTest: NextPageWithLayout<AWSTestPageProps> = ({ user }) => {
   const [value, setValue] = useState<File[]>([]);
+  const [uploading, setUploading] = useState(false);
 
   const handleFormSubmit = async (event: any) => {
     event.preventDefault();
-    console.log(value);
+
+    setUploading(true);
+
+    if (!value.length) {
+      console.log("No file selected");
+      return;
+    }
+
+    const file = value[0];
+
+    // This will need to be `listings/${listing.id}/images/${listing.id}-${file.name}`;
+    const key = `users/${user?.id}/listings/${user?.id}-${file.name}`;
+
+    const uploadUrl = (
+      await axios.get(`/api/aws/getPresignedUploadUrl?key=${key}`)
+    ).data.url;
+
+    // upload the file to the presigned URL
+    const response = await fetch(uploadUrl, {
+      method: "PUT",
+      body: file,
+      headers: {
+        "Content-Type": file.type,
+      },
+    });
+
+    if (!response.ok) {
+      console.log("Upload failed", response);
+      setUploading(false);
+    } else {
+      console.log("Upload successful");
+      setUploading(false);
+    }
   };
 
   return (
@@ -41,15 +79,17 @@ const AWSTest: NextPageWithLayout = () => {
           placeholder="Select images"
           label="Listing images"
           icon={<IconUpload size={rem(14)} />}
-          description=".png, .jpg, .jpeg, .webp"
           withAsterisk
           multiple
           accept="image/png,image/jpeg"
           value={value}
           onChange={setValue}
+          sx={{ marginBottom: "1rem", marginTop: "1rem" }}
         />
 
-        <button type="submit">Submit</button>
+        <Button type="submit" loading={uploading}>
+          Submit
+        </Button>
       </form>
     </>
   );
