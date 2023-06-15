491d62005d273f08e8eb142c58042409b0c8fd99
Hasan Iqbal <swapniliqbal@gmail.com>
2023-06-15 00:27:08 -0400

    fix: better handling of methods other than GET

--- pages/api/listings/recent.ts
+++ pages/api/listings/recent.ts
@@ -3,7 +3,7 @@ import { Image as PrismaImage, Listing as PrismaListing } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 const logger = require("@/utils/logger"); // Import the logger middleware using the 'require' syntax
 
-interface Image extends PrismaImage {}
+interface Image extends PrismaImage { }
 interface ListingWithImages extends PrismaListing {
   images: Image[];
 }
@@ -30,32 +30,33 @@ export default async function Handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  await logger(req, res, async () => {
-    // If the request method is HEAD, only send the status
+  logger(req, res, async () => {
     if (req.method === "HEAD") {
       res.status(200).end();
       return;
     }
 
-    if (req.method === "GET") {
-      try {
-        const recentAds = await getRecentListings();
+    if (req.method !== "GET") {
+      res.status(405).send({ error: "405 - Method not allowed" })
+      return;
+    }
 
-        if (!recentAds || recentAds.length === 0) {
-          res.status(200).send([]);
-          return;
-        }
+    try {
+      const recentAds = await getRecentListings();
 
-        // If the request method is not HEAD, send the user data
-        res.status(200).json(recentAds);
-      } catch (error: unknown) {
-        if (error instanceof Error) {
-          res.status(500).send({ error: error.message });
-        } else {
-          res.status(500).send({
-            error: "500 - An unexpected error occured.",
-          });
-        }
+      if (!recentAds || recentAds.length === 0) {
+        res.status(200).send([]);
+        return;
+      }
+
+      res.status(200).json(recentAds);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        res.status(500).send({ error: error.message });
+      } else {
+        res.status(500).send({
+          error: "500 - An unexpected error occured.",
+        });
       }
     }
   });
